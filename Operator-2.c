f5e87b5428dd1533f333e29a4d4b23c6f5f94a18
soov123456 <141641303+soov123456@users.noreply.github.com>
2023-08-09 15:50:43 +0900

    Add files via upload

---
+++ Operator-2.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//증산연산자
+	//int num1 = 10, num2 = 0;
+
+	//printf("num1 = %d, num2 = %d\n", num1, num2);      // num1 = 10, num2 = 0
+	//printf("++num1 = %d, num2 = %d\n", ++num1, num2);  // num1 = 11, num2 = 0
+
+	//num2 = ++num1 + 10; // num2 = 22
+	//printf("num1 = %d, num2 = %d\n", num1, num2);      // num1 = 12, num2 = 22
+
+	//printf("num1++ = %d, num2 = %d\n", num1++, num2);  // num1 = 12, num2 = 22
+	//printf("num1 = %d, num2 = %d\n", num1, num2);      // num1 = 13, num2 = 22
+
+	//num2 = num1++ + 10; // num2 = 23
+	//printf("num1 = %d, num2 = %d\n", num1, num2);      // num1 = 14, num2 = 23
+	//num2++; // num2 = 23
+	//printf("num1 = %d, num2 = %d\n", num1, num2);      // num1 = 14, num2 = 24
+
+
+	//int num1 = 10, num2 = 20;
+	//int result1, result2, result3, result4;
+
+	//result1 = num1 == num2; // 0
+	//result2 = num1 <= num2; // 1
+	//result3 = num1 > num2;  // 0 
+	//result4 = num1 != num2; // 1 
+	//// ! = not("아니"라는 뜻)
+	//printf("result1: %d\n", result1);
+	//printf("result2: %d\n", result2);
+	//printf("result3: %d\n", result3);
+	//printf("result4: %d\n", result4);
+
+
+	//논리연산자
+	//int num1 = 10, num2 = 20;
+	//int result1, result2, result3;
+
+	//result1 = num1 == 10 && num2 == 20; // && = and(그리고)
+	//result2 = num1 < 30 || num2 > 30; // || = or(둘중하나)
+	//result3 = !num1; // ! = not(아님)
+
+	//printf("result1: %d\n", result1); // 1
+	//printf("result2: %d\n", result2); // 1
+	//printf("result3: %d\n", result3); // 0
+
+
+	//int num1, num2;
+	//int result;
+
+	//printf("정수 입력1: "); // 12
+	//scanf("%d", &num1);     // num1 = 12
+
+	//printf("정수 입력2: "); // 8
+	//scanf("%d", &num2);     // num2 = 8
+
+	//result = num1 + num2;   // result = 20
+	//printf("%d + %d = %d\n", num1, num2, result); // 12 + 8, 20
+
+	////scanf = 저장(입력한 값을 변수에 저장)
+
+	
+	/*int num1, num2;
+	int result;
+
+	printf("정수 2개 입력: ");
+	scanf("%d %d", &num1, &num2);
+
+	result = num1 + num2;
+	printf("%d + %d = %d\n", num1, num2, result);*/
+
+
+	// 10진수, 2진수
+	//int num1, num2, num3;
+	//num1 = 27 // 00000000 00000000 00000000 00011011
+	//num2 = 189 // 00000000 00000000 00000000 10111101
+ //   num3 = 141 // 00000000 00000000 00000000 10001101
+
+
+	/*int num1 = 12;
+	int num2 = 0xb6;
+	printf("num1 = %d, %x\n", num1, num1);
+	printf("num2 = %d, %x\n", num2, num2);*/
+
+
+	// int num1 = 15; // 00000000 00000000 00000000 00001111
+	//           -15  =  11111111 11111111 11111111 11110001
+
+
+}
