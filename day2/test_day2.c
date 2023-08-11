c762c28ff7ce6a5764e7e74fc7be7c8c96344c57
soov123456 <141641303+soov123456@users.noreply.github.com>
2023-08-11 15:37:32 +0900

    Add files via upload

---
+++ day2/test_day2.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	//	for (int dan = 2; dan < 10; dan++) {
+//	//		for (int i = 1; i < 10; i++) {
+//	//			printf("%d x %d = %d\n", dan, i, dan * i);
+//	//		}
+//	//		printf("---------------------\n");
+//	//	}
+//
+//
+//	/*int num1;
+//	const int num2 = 55;
+//	num1 = num2 + 10;
+//		printf("%d\n", num1);*/
+//
+//
+//	//int sum = 0;
+//	//int i = 1;
+//
+//	//while(i < 10){
+//	//	sum += i;
+//	//	i++;
+//	//}
+//	//printf("%d\n", sum); // 45
+//
+//
+//	//int i = 1;
+//	//int sum = 0;
+//
+//	//do {
+//	//	sum += i;
+//	//	i++;
+//	//} while (i < 20);
+//	// 
+//	//	printf("%d\n", sum); // 190
+//
+//
+//	/*int sum = 0;
+//
+//	for (int i = 1; i <= 17; i++) {
+//		sum += i;
+//	}
+//	printf("%d\n", sum);*/
+//
+//
+//	/*int i = 1;
+//	int sum = 0;
+//
+//	while (i < 23) {
+//		sum += i;
+//		i++;
+//		
+//	}
+//	printf("%d\n", sum);*/
+//
+//	/*for (int dan = 2; dan < 19; dan++) {
+//		for (int i = 1; i <= 9; i++) {
+//			printf("%d X %d = %d\n", dan, i, dan * i);
+//		}
+//		printf("--------------\n");
+//	}*/
+//
+//	// 다음 해야할것 : if 반복문, if else 구조, 내일 예습
+//
+//
+//	/*for (int i = 1; i <= 100; i++) {
+//		if (i % 3 == 0 || i % 4 == 0) {
+//			printf("%d\n", i);
+//		}
+//	}*/
+//
+//
+//	/*int num;
+//
+//	printf("숫자를 입력해 주세요. 짝수인지 홀수인지 알려드립니다. : ");
+//	scanf("%d", &num);
+//
+//	if (num % 2 == 0) {
+//		printf("입력하신 숫자는 짝수입니다.\n", num);
+//	}
+//	else {
+//		printf("입력하신 숫자는 홀수입니다\n", num);
+//	}*/
+//
+//
+//	/*int num;
+//
+//	printf("4 X 2 의 정답은? : \n");
+//	scanf("%d", &num);
+//
+//	if (num == 8) {
+//		printf("정답입니다.");
+//	}
+//	else {
+//		printf("오답입니다");
+//	}*/
+//
+//
+//	//char select;
+//	//double num1, num2;
+//
+//	//printf("A:덧셈 B:뺄셈 C:곱셈 D:나눗셈\n");
+//	//printf("선택하여 알파벳을 입력해주세요 : \n");
+//	//scanf("%c", &select);
+//
+//	//printf("계산에 사용할 숫자 2개를 입력해 주세요. : \n");
+//	//scanf("%lf %lf", &num1, &num2);
+//
+//	//switch (select) {
+//	//case 'A':
+//	//case 'a':
+//	//	printf("%lf + %lf = %lf\n", num1, num2, num1 + num2);
+//	//	break;
+//	//case 'B':
+//	//case 'b':
+//	//	printf("%lf - %lf = %lf\n", num1, num2, num1 - num2);
+//	//	break;
+//	//case 'C':
+//	//case 'c':
+//	//	printf("%lf * %lf = %lf\n", num1, num2, num1 * num2);
+//	//	break;
+//	//case 'D':
+//	//case 'd':
+//	//	printf("%lf / %lf = %lf\n", num1, num2, num1 / num2);
+//	//	break;
+//	//}
+//
+//char i = 131;
+//
+//printf("%d\n", i);
+//
+//}
